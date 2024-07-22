540f457f2749c3169619168514fff3254f20aa7c
miguelruizc <ruizconde.m@gmail.com>
2024-07-22 18:13:36 +0200

    Add login & register forms with basic POST routes

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static('public'));
+// Directories setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.status(200).render('index');
@@ -15,8 +19,14 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
 	res.status(200).render('login');
 });
+app.post('/login', (req, res) => {
+	res.send(`POST /Login route hit with data: ${JSON.stringify(req.body)}`);
+});
 app.get('/register', (req, res) => {
 	res.status(200).render('register');
+});
+app.post('/register', (req, res) => {
+	res.send(`POST /Register route hit with data: ${JSON.stringify(req.body)}`);
 });
 app.get('/*', (req, res) => {
 	res.status(404).render('404');
