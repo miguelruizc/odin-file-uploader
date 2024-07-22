4ae1efcb4b361b918711c6d66be2d29246b7a8d4
miguelruizc <ruizconde.m@gmail.com>
2024-07-22 17:23:23 +0200

    Add initial navigation and basic styles

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 	res.status(200).render('index');
+});
+app.get('/login', (req, res) => {
+	res.status(200).render('login');
+});
+app.get('/register', (req, res) => {
+	res.status(200).render('register');
+});
+app.get('/*', (req, res) => {
+	res.status(404).render('404');
 });
 
 app.listen(PORT, () => {
