ed90b3c005c058ceddfeae11c91300c45cda78d1
miguelruizc <ruizconde.m@gmail.com>
2024-08-14 19:20:04 +0200

    Add path to each file for download

--- routes/upload.js
+++ routes/upload.js
@@ -1,34 +1,63 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../utils/multerSetup');
+const path = require('node:path');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 router.post('/', upload.single('file'), async (req, res) => {
 	const file = req.file;
-	let folderID = isNaN(parseInt(req.body.folder))
+	let folderId = isNaN(parseInt(req.body.folder))
 		? null
 		: parseInt(req.body.folder);
 
-	if (!(folderID === null)) {
+	// Validation/sanitization of folderID
+	if (!(folderId === null)) {
 		const validFolder = await prisma.folder.findUnique({
 			where: {
-				id: folderID,
+				id: folderId,
 				userId: req.user.id,
 			},
 		});
-		if (!validFolder) folderID = null;
+		if (!validFolder) folderId = null;
 	}
-
+	// Validation of file inputed
 	if (!file) {
 		console.log('Error in POST request to /upload: NO FILE');
 		return res.redirect('/');
 	}
 
-	console.log('File recieved via POST request in /upload:');
-	console.log('File: ', req.file);
-	console.log('Selected folder: ', folderID);
-	res.redirect('/');
+	// Add entry to DB
+	if (req.isAuthenticated()) {
+		try {
+			const filepath = path.join(
+				path.resolve(__dirname, '../'),
+				'uploads',
+				req.file.filename
+			);
+			const addedFile = await prisma.file.create({
+				data: {
+					name: req.file.filename,
+					user: {
+						connect: { id: req.user.id },
+					},
+					...(folderId && {
+						folder: {
+							connect: { id: folderId },
+						},
+					}),
+					path: filepath,
+				},
+			});
+			console.log('File uploaded and entry added to DB:\n', addedFile);
+			return res.redirect('/');
+		} catch (error) {
+			console.error('Error updating DB with new File: ', error);
+			return res.redirect('/');
+		}
+	} else {
+		return res.redirect('/');
+	}
 });
 
 module.exports = router;
