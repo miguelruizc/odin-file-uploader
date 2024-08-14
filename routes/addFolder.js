2827467723433da1e2fea3526f2d1e7312be01ec
miguelruizc <ruizconde.m@gmail.com>
2024-08-14 14:29:35 +0200

    Add functionality to create folders associated with users

--- routes/addFolder.js
+++ routes/addFolder.js
@@ -2,22 +2,43 @@ const express = require('express');
 const router = express.Router();
 const { folderNameValidator } = require('../utils/validation');
 const { validationResult } = require('express-validator');
+const { PrismaClient } = require('@prisma/client');
 
-router.post('/', folderNameValidator(), (req, res) => {
-	let errors = validationResult(req)
-		.array()
-		.map((error) => error.msg);
+const prisma = new PrismaClient();
 
-	if (errors.length > 0) {
-		console.log('Errors in /addFolder POST request: ');
-		console.log(errors);
-		return res.redirect('/invalidFolderName');
-		// TODO: decide if reload form and show errors or just redirect to '/'
-	}
+router.post('/', folderNameValidator(), async (req, res) => {
+	if (req.isAuthenticated()) {
+		let errors = validationResult(req)
+			.array()
+			.map((error) => error.msg);
 
-	console.log('POST request to /addFolder with data: ');
-	console.log('FolderName: ', req.body.folderName);
-	res.redirect('/');
+		if (errors.length > 0) {
+			console.log('Errors in /addFolder POST request: ');
+			console.log(errors);
+			return res.redirect('/'); // If form sanitization is invalid, redirect to /
+		}
+
+		// If form sanitization is valid:
+		console.log('POST request to /addFolder with data: ');
+		console.log('FolderName: ', req.body.folderName);
+		console.log('User: ', req.user);
+		try {
+			const folder = await prisma.folder.create({
+				data: {
+					name: req.body.folderName,
+					user: {
+						connect: { id: req.user.id },
+					},
+				},
+			});
+			console.log('Folder created: ', folder);
+
+			return res.redirect('/');
+		} catch (error) {
+			console.error('Error creating Folder item in database: ', error);
+			return res.redirect('/');
+		}
+	} else return res.redirect('/');
 });
 
 module.exports = router;
