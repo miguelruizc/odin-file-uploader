6ff293645c8264ae8be59b322347872fb75d39ff
miguelruizc <ruizconde.m@gmail.com>
2024-08-21 16:37:40 +0200

    Complete update/delete functionality for folders and files

--- controllers/delete.js
+++ controllers/delete.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const fs = require('node:fs');
 
 const POST_delete_folder = async (req, res) => {
 	// Check if user is authenticated
@@ -21,12 +22,16 @@ const POST_delete_folder = async (req, res) => {
 	}
 
 	// Delete folder and associated files from db
+	const filesToDelete = await prisma.file.findMany({
+		where: {
+			folderId,
+		},
+	});
 	const deletedFiles = await prisma.file.deleteMany({
 		where: {
 			folderId,
 		},
 	});
-	// TODO: Remove physical files from the server
 	const deletedFolder = await prisma.folder.delete({
 		where: {
 			id: folderId,
@@ -35,11 +40,60 @@ const POST_delete_folder = async (req, res) => {
 	console.log('Deleted folder: ', deletedFolder);
 	console.log('and associated files: ', deletedFiles);
 
+	// Remove physical files from the server
+	filesToDelete.forEach((file) => {
+		fs.unlink(file.path, (error) => {
+			if (error) {
+				console.error('Error deleting file from the filesystem: ', error);
+			} else {
+				console.log('File removed from the filesystem successfully');
+			}
+		});
+	});
+
 	res.redirect('/');
 };
 
-const POST_delete_file = (req, res) => {
-	console.log('POST request received in /delete/file/:id');
+const POST_delete_file = async (req, res) => {
+	// Check if user is authenticated
+	if (!req.isAuthenticated()) return res.redirect('/login');
+
+	// Check if the file exists and belongs to user
+	const fileId = parseInt(req.params.id);
+
+	const file = await prisma.file.findUnique({
+		where: {
+			id: fileId,
+		},
+	});
+
+	console.log('fileId: ', fileId);
+	console.log('File: ', file);
+
+	if (!file || file.userId !== req.user.id) {
+		console.log(
+			"Error deleting file: File doesn't exist or doesn't belong to authenticated user"
+		);
+		return res.redirect('/');
+	}
+
+	// Delete file from db
+	const deletedFile = await prisma.file.delete({
+		where: {
+			id: fileId,
+		},
+	});
+	console.log('Deleted file: ', deletedFile);
+
+	// Remove physical file from the server
+	fs.unlink(deletedFile.path, (error) => {
+		if (error) {
+			console.error('Error deleting file from the filesystem: ', error);
+		} else {
+			console.log('File removed from the filesystem successfully');
+		}
+	});
+
 	res.redirect('/');
 };
 
